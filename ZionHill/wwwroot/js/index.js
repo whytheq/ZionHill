528dfddb01c8eb328ff0e2314ea6e1498d1c48a3
Jason <whytheq@gmail.com>
2021-09-09 23:30:16 +0100

    Further work on css - needs so much more practise as has not yet clicked.

--- ZionHill/wwwroot/js/index.js
+++ ZionHill/wwwroot/js/index.js
@@ -1,18 +1,23 @@
-var x = 0;
-var s = "hello pluralsight"
+$(document).ready(function () {
+
+    var x = 0;
+    var s = "hello pluralsight"
 
 
-var button =$("#ToggleFormButton");
-button.on("click", function () {
-    if (!$("#formdiv").is(":visible")) {
-        $("#formdiv").show(2000)
-    } else {
-        $("#formdiv").hide("slow")
-    }
-})
+    var button = $("#ToggleFormButton");
+    button.on("click", function () {
+        if (!$("#formdiv").is(":visible")) {
+            $("#formdiv").show(3000)
+        } else {
+            $("#formdiv").hide("slow");
+        }
+    });
 
 
-var plantInfo = $(".specialist-plants li");
-plantInfo.on("click", function () {
-    console.log("you clicked on " + $(this).text())
-})
+
+    var plantInfo = $(".specialist-plants li");
+    plantInfo.on("click", function () {
+        console.log("you clicked on " + $(this).text())
+    })
+
+});
