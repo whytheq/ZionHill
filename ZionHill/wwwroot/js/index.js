a39f95adcd953dedc134cf678130caf590de33df
Jason <whytheq@gmail.com>
2021-09-10 08:50:09 +0100

    Playing with jQuery

--- ZionHill/wwwroot/js/index.js
+++ ZionHill/wwwroot/js/index.js
@@ -13,11 +13,37 @@ $(document).ready(function () {
         }
     });
 
+    var butt = $("#toggleItem");
+    butt.on("click", function () {
+        $("#loginBlock").slideToggle();
+    });
+
+
+
+    //var loginToggle = $("#login-block #toggleItem");
+    //var popupForm = $("#login-block .popup-form");
+
+    //loginToggle.on("click", function () {
+    //    popupForm.hide();
+    //    //console.log("you clicked on ");
+    //});
+
+    //<div id="login-block">
+    //    <div id="toggleItem">Login</div>
+    //    <div class="popup-form">
+
+
+
 
 
     var plantInfo = $(".specialist-plants li");
     plantInfo.on("click", function () {
         console.log("you clicked on " + $(this).text())
-    })
+    });
+
+
+
+
+
 
 });
